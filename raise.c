425e14b78e47035d6dd5997d09ed5ccfdff2d629
Kodai Hanadate <khanadat@c4r1s9.42tokyo.jp>
2025-06-25 09:47:43 +0900

    drawing

--- raise.c
+++ raise.c
@@ -4,8 +4,8 @@
 #include <X11/keysym.h>
 #include "minilibx/mlx.h"
 
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 2160
+# define WINDOW_WIDTH 1200
+# define WINDOW_HEIGHT 675
 # define MLX_ERROR 1
 
 typedef struct s_data
@@ -43,13 +43,12 @@ int	main(void)
 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
 	if (!data.win_ptr)
 		return (free(data.mlx_ptr), MLX_ERROR);
-	mlx_look_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
 
 	mlx_loop(data.mlx_ptr);
 
 	mlx_destroy_display(data.mlx_ptr);
-	free(data.mlx_ptr);
-	return (0);
+	return (free(data.mlx_ptr), 0);
 }
