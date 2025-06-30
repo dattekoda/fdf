525aab7be8c7a3a0c2a4c5c4a4e569e6236d1d30
Kodai Hanadate <khanadat@c1r5s1.42tokyo.jp>
2025-06-30 19:33:22 +0900

    stable rectangle

--- test/test3.c
+++ test/test3.c
@@ -1,5 +1,20 @@
 #include "test.h"
 
+int	encode_rgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+	return (red << 16 | green << 8 | blue);
+}
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
 void	img_pix_put(t_img *img, int x, int y, int color)
 {
 	char	*pixel;
@@ -8,19 +23,85 @@ void	img_pix_put(t_img *img, int x, int y, int color)
 	*(int *)pixel = color;
 }
 
-void	img_pix_put(t_img *img, int x, int y, int color)
-{
-	char	*pixel;
-	int		i;
+// void	img_pix_put(t_img *img, int x, int y, int color)
+// {
+// 	char	*pixel;
+// 	int		i;
 
-	i = img->bpp - 8;
-	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	while (0 <= i)
+// 	i = img->bpp - 8;
+// 	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+// 	while (0 <= i)
+// 	{
+// 		if (img->endian != 0) //big endian = bigger value from right
+// 			*pixel++ = (color >> i) & 0xFF;
+// 		else //litttle endian = bigger value from left
+// 			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+// 		i -= 8;
+// 	}
+// }
+
+int	render_rect(t_img *img, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	i = rect.y;
+	while (i <rect.y + rect.height)
 	{
-		if (img->endian != 0) //big endian = bigger value from right
-			*pixel++ = (color >> i) & 0xFF;
-		else //litttle endian = bigger value from left
-			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
-		i -= 8;
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;
 	}
+	return (0);
+}
+
+void	render_background(t_img *img, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			img_pix_put(img, j++, i, color);
+		++i;
+	}
+}
+
+int	render(t_data *data)
+{
+	int	range;
+
+	range = 700;
+	if (data->win_ptr == NULL)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){WINDOW_WIDTH - range, WINDOW_HEIGHT - range, range, range, GRE_PIXEL});
+	render_rect(&data->img, (t_rect){0, 0, range, range, RED_PIXEL});
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
+	if (!data.win_ptr)
+		return (free(data.win_ptr), MLX_ERROR);
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
+	mlx_destroy_display(data.mlx_ptr);
+	return (free(data.mlx_ptr), 0);
 }
